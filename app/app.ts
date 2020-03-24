e2ad5d4d45e2d3761913043f2c4397e27078426a
Oswar <moises.cruzn499@gmail.com>
2020-03-23 22:41:46 -0600

    se modifico app.ts para ingresar los documentos a mongo

--- app/app.ts
+++ app/app.ts
@@ -4,4 +4,4 @@ import { createVehiculo } from "./vehiculo"
 createSucursal("Sucursal2", "Honduras", "Francisco Morazan", "Tegucigalpa", "Direccion Exacta .jpg")
 async function wg(){ const suc:any = await getSucursal("Sucursal1");console.log(suc);} wg()
 
-//createVehiculo("Sucursal1","BAF4087", "Camioneta", "Toyota", "Prado", "Negro", "KJHUI89786567", 2000)
+createVehiculo("Sucursal1","BAF4087", "Camioneta", "Toyota", "Prado", "Negro", "KJHUI89786567", 2000)
