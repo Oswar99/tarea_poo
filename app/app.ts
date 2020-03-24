89dd8959868d810ccfe0d3ff20e8559854c3705e
Oswar <moises.cruzn499@gmail.com>
2020-03-23 22:45:51 -0600

    se modifico app.ts para comentar  funcion que lee y imprime una sucursal el la consola

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import { createSucursal, getSucursal } from "./sucursal"
 import { createVehiculo } from "./vehiculo"
 
-createSucursal("Sucursal2", "Honduras", "Francisco Morazan", "Tegucigalpa", "Direccion Exacta .jpg")
-async function wg(){ const suc:any = await getSucursal("Sucursal1");console.log(suc);} wg()
-
+createSucursal("Sucursal1", "Honduras", "Francisco Morazan", "Tegucigalpa", "Direccion Exacta .jpg")
+//async function wg(){ const suc:any = await getSucursal("Sucursal1");console.log(suc);} wg()
 createVehiculo("Sucursal1","BAF4087", "Camioneta", "Toyota", "Prado", "Negro", "KJHUI89786567", 2000)
